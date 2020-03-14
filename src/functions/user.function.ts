25ed0a48453cd4afb38a204afd097e5957c192d6
hurui <huruixyz@gmail.com>
2020-03-14 21:50:57 +0800

    refactor(user.function): use better variable names

--- src/functions/user.function.ts
+++ src/functions/user.function.ts
@@ -3,9 +3,9 @@ import { UserNotExistException } from '../exceptions/user.exception'
 import User from '../models/user.model'
 
 export async function getUserInfo(): Promise<User> | never {
-  const user = cloud.database().collection('users')
-  const res = await user.get()
-  const users = res.data as User[]
-  if (!!users) throw new UserNotExistException()
-  return users[0]
+  const users = cloud.database().collection('users')
+  const res = await users.get()
+  const user = (res.data as User[])[0]
+  if (!!user) throw new UserNotExistException()
+  return user
 }
